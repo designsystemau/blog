380006b9f6117a57e9041a100910cb07d2189983
Petra Gulicher <p.gulicher@gmail.com>
2021-08-12 11:52:40 +1000

    debuggin postprocessing

--- .github/workflows/repo-data-response.js
+++ .github/workflows/repo-data-response.js
@@ -2,23 +2,26 @@ const decoder = new TextDecoder("utf-8");
 const encoder = new TextEncoder();
 
 let json;
+let response;
 
 try {
   json = await Deno.readFile(".github/workflows/repo-data-response.json");
-  json = decoder.decode(json);
+  response = JSON.parse(decoder.decode(json));
 } catch (error) {
   console.log("There was an error parsing the JSON response:", error);
 }
 
-if (json?.errors?.length) {
+if (response?.errors?.length) {
   console.log(
     "There was an error response from the API:",
-    JSON.stringify(json.errors)
+    JSON.stringify(response.errors)
   );
 } else {
   try {
-    await Deno.writeFile("src/data/repo-data.json", encoder.encode(json.data));
-    console.log("Saved response data:", encoder.encode(json.data));
+    await Deno.writeFile(
+      "src/data/repo-data.json",
+      encoder.encode(JSON.stringify(response.data))
+    );
   } catch (error) {
     console.log("There was an error writing your JSON file:", error);
   }
